13ca97e145005b2ba1e6ee24e7b6872387b47a69
Sylwester Gruszka <gruszex@gmail.com>
2020-10-07 22:19:07 +0200

    Now using and updating original stackfile (as opposed to overwriting it with local stackfile from repo)

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # gitlab-portainer-deploy
 
+This tool makes it easy to deploy from CI to Portainer, without loosing original
+compose file (stackfile) contents in Portainer.
+
+All it does is read the compose file, replace the image of given service 
+(in a very naive way) and shove updated compose file back into Portainer.
+
+This fork is based on vvarp's gitlab-portainer-deploy, which doesn't preserve compose-file
+and requires local compose file in repository.
+
 ## Usage
 
 In `.gitlab-ci.yml`:
@@ -7,15 +16,37 @@ In `.gitlab-ci.yml`:
 ```yaml
 deploy:
   stage: deploy
-  image: vvarp/gitlab-portainer-deploy
+  image: gruszex/gitlab-portainer-deploy
   variables:
     PORTAINER_URL: ""
     PORTAINER_USERNAME: ""
     PORTAINER_PASSWORD: ""
-    PORTAINER_STACK: ""
-    STACKFILE: ""
   script:
-    - deploy -e SOME_STACKFILE_VAR=value -e ANOTHER_STACKFILE_VAR=value
+    - deploy --stack_name=x --service-name=y --new-image=z:1.0 -e SOME_STACKFILE_VAR=value
   tags:
     - docker
 ```
+
+## Why
+
+Rationale for this kind of behaviour is not to loose, often complex,
+compose file from eyesight and to still be able to update it manually
+inside Portainer. Also not to have to sync common parts of compose file (e.g.
+networks, volumes, anchors) between multiple repositories that push 
+services to the same stack.
+
+The reason for not parsing yaml is keeping anchors and links untouched.
+Yaml parsers expand anchors and links, destroying original compose file 
+(ruamel.yaml was supposed to manage this without expanding them
+but failed with real world compose files).
+
+This introduced a constraint to compose yaml: `image` must be specified as first item
+in service definition.
+
+## Local development
+
+For easy local testing, make a virtualenv and install the code
+with the following command:
+`pip install --editable . `
+
+From now on you have `deploy` command available in this env.
--- deploy/cli.py
+++ deploy/cli.py
@@ -1,5 +1,5 @@
 import json
-import os
+import re
 import sys
 
 import click
@@ -10,10 +10,12 @@ import requests
 @click.option("--portainer-url", envvar="PORTAINER_URL", required=True, help="Portainer instance URL")
 @click.option("--portainer-username", envvar="PORTAINER_USERNAME", required=True, help="Portainer username")
 @click.option("--portainer-password", envvar="PORTAINER_PASSWORD", required=True, help="Portainer password")
-@click.option("--portainer-stack", envvar="PORTAINER_STACK", default=None, required=True)
-@click.option("--stackfile", envvar="STACKFILE", default="docker-stack.yml", required=True)
+@click.option("--stack-name", envvar="STACK_NAME", default=None, required=True)
+@click.option("--service-name", envvar="SERVICE_NAME", default=None, required=True)
+@click.option("--new-image", envvar="NEW_IMAGE", default=None, required=True)
 @click.option("--env-var", "-e", multiple=True, default=[], required=False)
-def main(portainer_url, portainer_username, portainer_password, portainer_stack, stackfile, env_var):
+@click.option("--verbose", "-v", default=False, required=False)
+def main(portainer_url, portainer_username, portainer_password, stack_name, service_name, new_image, env_var, verbose):
     # Build list of env vars to be passed on to Portainer
     stack_env = []
     if len(env_var) > 0:
@@ -43,35 +45,66 @@ def main(portainer_url, portainer_username, portainer_password, portainer_stack,
     headers = {"Authorization": "Bearer " + auth.json()["jwt"]}
     click.echo(click.style(" done", fg="green"))
 
-    # Try to read stackfile contents
-    click.echo(click.style("Checking for stackfile...", fg="yellow"), nl=False)
-    if os.path.isfile(stackfile):
-        with open(stackfile) as f:
-            stackfilecontent = f.read()
-    else:
-        click.echo(click.style(" can't find stackfile", fg="red"))
-        sys.exit(1)
-
-    click.echo(click.style(" done", fg="green"))
-
     # Get IDs for target endpoint and stack
     click.echo(click.style("Getting target stack ID...", fg="yellow"), nl=False)
     stacks = requests.get(f"{portainer_url}/stacks", headers=headers)
     if stacks.status_code != 200:
-        click.echo(f"\nHTTP {auth.status_code} error while trying to get list of Portainer stacks")
+        click.echo(f"\nHTTP {stacks.status_code} error while trying to get list of Portainer stacks")
         sys.exit(1)
 
     stack_id = None
     for s in stacks.json():
-        if s["Name"] == portainer_stack:
+        if s["Name"] == stack_name:
             stack_id = str(s["Id"])
             endpoint_id = str(s["EndpointId"])
 
     if stack_id is None:
-        click.echo(click.style(f" can't find stack \"{portainer_stack}\" in Portainer", fg="red"))
+        click.echo(click.style(f" can't find stack \"{stack_name}\" in Portainer", fg="red"))
         sys.exit(1)
 
     click.echo(click.style(" done", fg="green"))
+
+    # Try to read stackfile contents
+    click.echo(click.style("Getting stackfile...", fg="yellow"), nl=False)
+    stackfile = requests.get(f"{portainer_url}/stacks/{stack_id}/file", headers=headers)
+    if stackfile.status_code != 200:
+        click.echo(f"\nHTTP {stackfile.status_code} error while trying to get stack compose file contents")
+        print(stackfile.request.url)
+        sys.exit(1)
+
+    stackfile_content = stackfile.json()["StackFileContent"]
+    updated_stackfile_lines = []
+    click.echo(click.style(" done", fg="green"))
+
+    # Now go through yaml lines and update image line of requested service
+    current_image = None
+    found_service = False
+    for line in stackfile_content.splitlines():
+        if found_service:  # Service definition started in previous line
+            if "image:" not in line:
+                click.echo(click.style(
+                    f"\nFirst line of service {service_name} definition was not `image` key. "
+                    f"Can't proceed with update.", fg="red"))
+                sys.exit(1)
+            whitespace, current_image = line.split("image:", maxsplit=1)
+            updated_stackfile_lines.append(f"{whitespace}image: {new_image}")
+            found_service = False
+            continue
+
+        # Mark service definition to know that in next line there should be image specified
+        # (yep, very naive approach but works, we just always specify image as first line
+        #  of service definition)
+        if re.match(f"^\\s+{service_name}:", line):
+            found_service = True
+        else:
+            found_service = False
+
+        updated_stackfile_lines.append(line)
+
+    if current_image is None:
+        click.echo(click.style(
+            f"\nService {service_name} definition was not found in stack yaml.", fg="red"))
+        sys.exit(1)
 
     # Update stack
     click.echo(click.style("Requesting stack update...", fg="yellow"), nl=False)
@@ -79,15 +112,21 @@ def main(portainer_url, portainer_username, portainer_password, portainer_stack,
         f"{portainer_url}/stacks/{stack_id}?endpointId={endpoint_id}",
         headers=headers,
         json={
-            "StackFileContent": stackfilecontent,
+            "StackFileContent": "\n".join(updated_stackfile_lines),
             "Env": stack_env,
             "Prune": False
         }
     )
     click.echo(click.style(" done", fg="green"))
 
-    click.echo(f"\nRequest to update stack finished with HTTP {r.status_code}: \n{json.dumps(r.json(), indent=4)}\n")
+    click.echo(f"\nRequest to update stack finished with HTTP {r.status_code}")
+    if verbose:
+        click.echo(f"\n{json.dumps(r.json(), indent=4)}")
 
     if r.status_code != 200:
         click.echo(click.style(f"Deployment failed", fg="red"))
         sys.exit(1)
+
+    click.echo(click.style(f"\nUpdated service {service_name}:"
+                           f"\n  from: {current_image.strip()}"
+                           f"\n    to: {new_image.strip()}", fg="green"))
